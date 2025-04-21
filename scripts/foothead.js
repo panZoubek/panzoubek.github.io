9f6daf347338460ef5b83b94e76beb35d7a4359f
Honza <jachym.nemecek@gmail.com>
2025-04-21 17:04:39 +0200

    new page lol

---
+++ scripts/foothead.js
@@ -0,0 +1,26 @@
+const futr = `
+    <div class="columns">
+        <div class="column">
+            <img src="feet.png" alt="2 foot" class="image is-2by1">
+        </div>
+    </div>
+    <div class="columns is-multiline has-text-centered ffff">
+        <div class="column is-one-quarter">
+            <p class="title">MADE WITH HTML, CSS AND <a href="https://bulma.io" class="my-a" target="_blank">BULMA</a></p>
+        </div>
+        <div class="column half">
+            <a href="" class="no-u"><p class="title" style="font-size: 3rem;">ABOUT JAKIMS</p></a>
+        </div>
+        <div class="column is-one-quarter">
+            <p class="title">© 2025 Jakims page is licensed under <a class="my-a" href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0</a></p>
+        </div>
+    </div>`;
+document.getElementById('FOOTER').insertAdjacentHTML("beforeend",futr)
+
+
+const hedr = `
+<div class="hero-head has-text-centered" style="display: flex; align-items: center; justify-content: center; ">
+    <a href="home.html"><p class="title has-text-centered" style="font-size: 4rem;"><u> Jakims </u></p></a>
+    <img src="kyti.png" class="image is-96x96" style="margin-left: 1rem">
+</div>`
+document.getElementById('HEADER').insertAdjacentHTML("afterbegin",hedr)
---
+++ scripts/novinky.js
@@ -0,0 +1,45 @@
+
+/* pro NEWS a HOME page*/
+/* nejnovejsi na zacatek (kdyby insert byl after begin tak naopak, ale je beforeend) */
+const novinecky = [
+    ["21.4.2025","WOW it works, this is awesome"],
+    ["21.4.2025","Working on the page"],
+    ["5.4.2025","Leošek and others posted"],
+    ["5.4.2025","Page created"],
+    ["5.4.2025","The sky went on fire"],
+]
+
+let tynews = document.getElementById('THENEWS')
+let newnews = document.getElementById('NEWESTNEWS')
+
+if (tynews != null) {
+    for (let i=0; i < novinecky.length; i++) {
+        let content = `
+        <div class="cell">
+            <div>
+                <h1 class="subtitle"><i class="date">${novinecky[i][0]}</i> – ${novinecky[i][1]}</h1>
+            </div>
+        </div>
+        `
+        tynews.insertAdjacentHTML("beforeend",content)
+    }
+}
+
+
+if (newnews != null ) {
+    const newestnews = `
+        <div class="cell" style="padding-bottom: 1rem;">
+            <p class="subtitle one_new"><i class="date">${novinecky[0][0]}</i> – ${novinecky[0][1]}</p>
+            <p class="subtitle one_new">|</p>
+            <p class="subtitle one_new"><i class="date">${novinecky[1][0]}</i> – ${novinecky[1][1]}</p>
+            <p class="subtitle one_new">|</p>
+            <p class="subtitle one_new"><i class="date">${novinecky[2][0]}</i> – ${novinecky[2][1]}</p>
+            <p class="subtitle one_new">|</p>
+            <p class="subtitle one_new"><a class="my-a" href="novinky.html">look into history ...</a></p>
+        </div>
+        `
+        newnews.insertAdjacentHTML("afterbegin",newestnews)
+}
+
+
+
