47a1fd129cbe1f7dbc30ebe30a12c65761ff6cd9
Honza <jachym.nemecek@gmail.com>
2026-02-12 20:01:29 +0100

    pfp update & img resize

--- scripts/gaming.js
+++ scripts/gaming.js
@@ -3,7 +3,8 @@
 const way_to_imgs = "steam_pfps/";
 
 const profiles = [
-    ["touch-my-soul.jpg", "touch my soul", ["current profile"]],
+    ["happy-last-1.jpg", "Happy Last -1", ["current profile"]],
+    ["touch-my-soul.jpg", "touch my soul", ["12.2.2026"]],
     ["distant-birb.jpg", "distant birb", ["10.2.2026-11.2.2026"]],
     ["Tears-are-falling.jpg", "Tears are falling", ["25.1.2026-9.2.2026"]],
     ["left-plus-right.jpg", "Left + Right", ["14.1.2026-24.1.2026"]],
@@ -16,16 +17,16 @@ const profiles = [
     ["i-did-it.jpg","I did it", ["14.9.2025"]],
     ["okilldoit.jpg", "Okay, I'll do it", ["10.9.2025-13.9.2025"]],
     ["lost-soup.jpg", "lost soup", ["29.8.2025-9.9.2025"]],
-    ["from-monday-to-today.png", "from monday to today", ["24.8.2025-28.8.2025"]],
-    ["so-cute.png", "so cute", ["17.8.2025-23.8.2025"]],
-    ["clippy-super-blush.png", "super blush", ["11.8.2025-17.8.2025"]],
+    ["from-monday-to-today.jpg", "from monday to today", ["24.8.2025-28.8.2025"]],
+    ["so-cute.jpg", "so cute", ["17.8.2025-23.8.2025"]],
+    ["clippy-super-blush.jpg", "super blush", ["11.8.2025-17.8.2025"]],
     ["clippy.jpg", "I like", ["11.8.2025-12.8.2025", '<a target="_blank" href="https://www.geekwire.com/2011/microsoft-brings-clippy-office-training-game/" class="my-a">img source</a>']],
     ["try-not-to-hate-yourself.jpg", "try not to hate yourself", ["8.8.2025-10.8.2025"]],
     ["salt-lamp-gaming.jpg", "salt lamp gaming", ["5.8.2025-7.8.2025"]],
     ["the-world-sees-me.jpg", "the world sees me", ["2.8.2025-4.8.2025", '<a target="_blank" href="https://www.vecteezy.com/vector-art/22024202-tom-and-jerry-cartoon" class="my-a">img source</a>']],
     ["sky.jpg", "sky", ["1.8.2025"]],
     ["pianowalker.jpg", "the piano walker", ["28.7.2025-31.7.2025"]],
-    ["gettingawaywithit.png", "getting away with it", ["19.7.2025-28.7.2025", '<a target="_blank" href="https://knowyourmeme.com/memes/he-cant-keep-getting-away-with-it" class="my-a">img source</a>']],
+    ["gettingawaywithit.jpg", "getting away with it", ["19.7.2025-28.7.2025", '<a target="_blank" href="https://knowyourmeme.com/memes/he-cant-keep-getting-away-with-it" class="my-a">img source</a>']],
     ["leosek.jpg", "nad", ["main pfp"]]
 ];
 // nejnovejsi na zacatku
