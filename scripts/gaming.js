ae2e449c16777d71a8b0dc7399cdf5e82593080f
Honza <jachym.nemecek@gmail.com>
2026-02-20 21:09:45 +0100

    pfp update

--- scripts/gaming.js
+++ scripts/gaming.js
@@ -3,7 +3,8 @@
 const way_to_imgs = "steam_pfps/";
 
 const profiles = [
-    ["Echos.jpg", "Echos", ["current profile"]],
+    ["last-letter.jpg", "last letter", ["current profile"]],
+    ["Echos.jpg", "Echos", ["14.2.2026-20.2.2026"]],
     ["happy-last-1.jpg", "Happy Last -1", ["13.2.2026"]],
     ["touch-my-soul.jpg", "touch my soul", ["12.2.2026"]],
     ["distant-birb.jpg", "distant birb", ["10.2.2026-11.2.2026"]],
